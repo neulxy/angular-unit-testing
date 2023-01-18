75b397c156a2fa72c83803aec9529fc7e9a03348
Xiangyu Liu <neulxy@gmail.com>
2023-01-19 10:04:59 +1300

    testing dependencies components and services

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
+import { UserService } from './user.service';
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -17,5 +18,30 @@ describe('UserComponent', () => {
 
   it('should create the app', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should use the user name from the service', () => {
+    let userService = fixture.debugElement.injector.get(UserService);
+    expect(userService.user.name).toEqual(component.user.name);
+  });
+
+  it('should display the user name if user is logged in', () => {
+    let userService = fixture.debugElement.injector.get(UserService);
+    component.isLoggedIn = true;
+    fixture.detectChanges();
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).toContain(
+      userService.user.name
+    );
+  });
+
+  it("should't display the user name if user is logged in", () => {
+    let userService = fixture.debugElement.injector.get(UserService);
+    component.isLoggedIn = false;
+    fixture.detectChanges();
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).not.toContain(
+      userService.user.name
+    );
   });
 });
