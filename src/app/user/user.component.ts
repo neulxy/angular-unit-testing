29886ac83851d77f35a0632129eaf7c7efea745a
Xiangyu Liu <xiangyu.liu@mitre10.co.nz>
2023-01-19 09:29:45 +1300

    WIP

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
+  providers: [UserService],
 })
 export class UserComponent implements OnInit {
   user: { name: string } = { name: '' };
   isLoggedIn = false;
 
-  constructor() {}
+  constructor(private userService: UserService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.user = this.userService.user;
+  }
 }
